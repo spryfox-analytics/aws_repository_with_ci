7ea66c90414a9dd3a3a90854c447fdf34e0accd3
Maximilian Walther <maximilian.walther@spryfox.de>
2025-07-11 12:22:20 +0200

    shorter names for all services

--- locals.tf
+++ locals.tf
@@ -1,22 +1,12 @@
 locals {
-  camel_cased_repository_path = replace(
-    title(
-      regexreplace(
-        var.gitlab_repository_path,
-        "[/\\-_]+",    # match one or more of slash, backslash, dash or underscore
-        " "            # replace them all with a single space
-      )
-    ),
-    " ",             # now remove the spaces
-    ""
-  )
-  dashed_repository_path = replace(
-      regexreplace(
-        var.gitlab_repository_path,
-        "[/\\-_]+",    # match one or more of slash, backslash, dash or underscore
-        "-"            # replace them all with a single space
-      ),
-    " ",             # now remove the spaces
-    ""
-  )
+  repository_pat_parts = split("/", var.gitlab_repository_path)
+  trimmed_repository_path    = element(local.repository_pat_parts, length(local.repository_pat_parts) - 1)
+  normalized_repository_path = replace(
+    replace(
+      replace(local.trimmed_repository_path, "/", " "),
+      "-", " "),
+    "_", " ")
+  titleized_repository_path = title(local.normalized_repository_path)
+  camel_cased_repository_path = replace(local.titleized_repository_path, " ", "")
+  dashed_repository_path = replace(local.normalized_repository_path, " ", "-")
 }
